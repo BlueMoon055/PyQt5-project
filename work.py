42632bcbc5ce59e7dc97990f9c599932106e5c1a
Daniil <danilabajkalov@gmail.com>
2026-02-22 20:16:26 +0300

    код приложения

--- work.py
+++ work.py
@@ -1,61 +1,61 @@
+# импортируем:
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QLineEdit, QHBoxLayout, QListWidget
 from PyQt5.QtGui import QPixmap, QBrush, QIcon
 
-def add_1():
-    tasks_text_edit.text()
-    list_left.addItem(tasks_text_edit.text())
+def add_1(): # функция для кнопки добавить (левый список)
+    tasks_text_edit.text() # изменить текст
+    list_left.addItem(tasks_text_edit.text()) # добавить элемент в список
 
-def delete_all_elements():
-    list_left.clear()
+def delete_all_elements(): # функция для кнопки удалить всё (левый список)
+    list_left.clear() # очистить лист
 
-def delete_1_element():
-    #удаляем выбранный элемент из списка
-    list_left.currentItem()
-    list_left.takeItem(list_left.currentRow())
-    list_left.setCurrentRow(-1)
+def delete_1_element(): # функция для кнопки удалить выбранную заметку (левый список)
+    list_left.currentItem() # выбрать элемент
+    list_left.takeItem(list_left.currentRow()) # удаляем выбранный элемент
+    list_left.setCurrentRow(-1) # выделить строку под номером -1
 
-def move_element_in_done():
-    a = list_left.currentItem()
+def move_element_in_done(): # функция для кнопи переместить в сделано
+    a = list_left.currentItem() # выбрать элемент
     if a:
-        it_text = a.text() #получаем текст элемента
-        list_left.takeItem(list_left.currentRow()) #удаляем из начального списка
-        list_right.addItem(it_text)
-        list_left.setCurrentRow(-1)
+        it_text = a.text() # получаем текст элемента
+        list_left.takeItem(list_left.currentRow()) # удаляем элемент из левого списка
+        list_right.addItem(it_text) # добавляем элемент в правый список
+        list_left.setCurrentRow(-1) # выделить строку под номером -1
 
-def move_element_in_tasks_list():
-    b = list_right.currentItem()
+def move_element_in_tasks_list(): # функция для кнопки переместить в список дел
+    b = list_right.currentItem() # выбрать элемент
     if b:
-        take_text = b.text()
-        list_right.takeItem(list_right.currentRow())
-        list_left.addItem(take_text)
-        list_right.setCurrentRow(-1)
+        take_text = b.text() # получаем текст элемента
+        list_right.takeItem(list_right.currentRow()) # удаляем элемент из правого списка
+        list_left.addItem(take_text) # добавляем элемент в левый список
+        list_right.setCurrentRow(-1) # выделить строку под номером -1
 
-def delete_all_elements_done():
-    list_right.clear()
+def delete_all_elements_done(): # функция для кнопки удалить всё (правый список)
+    list_right.clear() # очистить лист
 
-def delete_1_element_done():
-    list_right.currentItem()
-    list_right.takeItem(list_right.currentRow())
-    list_right.setCurrentRow(-1)
+def delete_1_element_done(): # функция для кнопки удалить выбранную заметку (правый список)
+    list_right.currentItem() # выбрать элемент
+    list_right.takeItem(list_right.currentRow()) # удалить выбранную заметку
+    list_right.setCurrentRow(-1) # выделить строку под номером -1
 
-app = QApplication([]) #создание объекта-приложения
-app.setWindowIcon(QIcon("иконка.jpg"))
-main_win = QWidget() #создание объекта-окна
-main_win.resize(700, 400) #задать размеры окна
-main_win.setWindowTitle('список дел') #задать название окну
-main_win.show() #сделать объект-окно видимым
+app = QApplication([]) # создание объекта-приложения
+app.setWindowIcon(QIcon("иконка.jpg")) # устанавливаем иконку для приложения
+main_win = QWidget() # создание объекта-окна
+main_win.resize(700, 400) # задать размеры окна
+main_win.setWindowTitle('список дел') # задать название окну
+main_win.show() # сделать объект-окно видимым
 
-palette = main_win.palette()
-palette.setBrush(main_win.backgroundRole(), QBrush(QPixmap('фон.png')))
-main_win.setPalette(palette)
-main_win.setStyleSheet("QPushButton, QLabel { background-color: pink; }") # Установить розовый цвет для виджетов
+palette = main_win.palette() # получаем текущую палитру главного окна для изменений
+palette.setBrush(main_win.backgroundRole(), QBrush(QPixmap('фон.png'))) # устанавливаем фон
+main_win.setPalette(palette) # применяем изменённую палитру к окну
+main_win.setStyleSheet("QPushButton, QLabel { background-color: pink; }") # устанавливаем розовый цвет для виджетов
 
-# виджеты
-list_left = QListWidget()
+# виджеты:
+list_left = QListWidget() # создание виджета списка
 list_right = QListWidget()
-tasks_text = QLabel('список дел:')
-delete_all_left = QPushButton('удалить все')
+tasks_text = QLabel('список дел:') # создание виджета с текстом
+delete_all_left = QPushButton('удалить все') # создание виджета кнопки
 delete_all_right = QPushButton('удалить все')
 delete1_left = QPushButton('удалить выбранную заметку')
 delete1_right = QPushButton('удалить выбранную заметку')
@@ -64,17 +64,17 @@ b_add = QPushButton('добавить')
 n2 = QPushButton('переместить в сделано')
 list_of_tasks = QPushButton('переместить  в список дел')
 done = QLabel('сделано:')
-tasks_text_edit = QLineEdit()
-new_edit = QLineEdit()
-done_edit = QLineEdit()
+tasks_text_edit = QLineEdit() # создание виджета для ввода текста
 
-main_line = QHBoxLayout() #создание горизонтальной направляющей линиии(главная)
-v_line1 = QVBoxLayout() #создание вертикальной линии
+main_line = QHBoxLayout() # создание горизонтальной направляющей линиии (главная)
+v_line1 = QVBoxLayout() # создание вертикальной направляющей линии
 v_line2 = QVBoxLayout()
 v_line3 = QVBoxLayout()
 h_line1 = QHBoxLayout()
 h_line2 = QHBoxLayout()
-v_line1.addWidget(tasks_text, alignment=Qt.AlignCenter)#добавление виджета на линию
+
+# добавление виджетов на линии:
+v_line1.addWidget(tasks_text, alignment=Qt.AlignCenter)
 v_line1.addWidget(list_left, alignment=Qt.AlignCenter)
 h_line1.addWidget(delete_all_left, alignment=Qt.AlignCenter)
 h_line1.addWidget(delete1_left, alignment=Qt.AlignCenter)
@@ -92,11 +92,13 @@ h_line2.addWidget(delete_all_right, alignment=Qt.AlignCenter)
 h_line2.addWidget(delete1_right, alignment=Qt.AlignCenter)
 v_line3.addLayout(h_line2)
 
+# загружаем линии на главную линию:
 main_line.addLayout(v_line1)
 main_line.addLayout(v_line2)
 main_line.addLayout(v_line3)
-main_win.setLayout(main_line) #загрузка заполненной линии на окно приложения
+main_win.setLayout(main_line) # загрузка заполненной линии на окно приложения
 
+# подключаем кнопки:
 b_add.clicked.connect(add_1)
 delete_all_left.clicked.connect(delete_all_elements)
 delete1_left.clicked.connect(delete_1_element)
